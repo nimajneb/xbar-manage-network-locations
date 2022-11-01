46dcfc7454136894829765dcad837e9ce4bc485a
benjamin <benjamin@neugelb.com>
2022-11-01 16:48:48 +0100

    Support for add/delete operations

--- switch-network-location.1s.sh
+++ switch-network-location.1s.sh
@@ -1,28 +1,52 @@
 #!/usr/bin/env bash
 
-# <xbar.title>Network location switcher</xbar.title>
+# <xbar.title>Network location manager</xbar.title>
 # <xbar.version>v1.0</xbar.version>
 # <xbar.author>Benjamin Wilfing <hallo@nimajneb.dev></xbar.author>
 # <xbar.author.github>nimajneb</xbar.author.github>
-# <xbar.desc>Switch your network location quickly</xbar.desc>
-# <xbar.dependencies>bash</xbar.dependencies>
+# <xbar.desc>Manage and network locations and switch them quickly</xbar.desc>
+# <xbar.dependencies>bash,osascript</xbar.dependencies>
 
 # <xbar.var>string(VAR_SYMBOL="ᯤ"): Symbol/Character used as a prefix in the menu bar</xbar.var>
+
+IFS=$'\n'
 
 current_location=$(networksetup -getcurrentlocation)
 all_locations=$(networksetup -listlocations)
 
-echo "${VAR_SYMBOL} ${current_location}"
-echo "---"
-echo "Switch location"
-echo "---"
-IFS=$'\n'
-for location in $all_locations
-do
-    entry_prefix=" "
-    if [ "${location}" == "${current_location}" ]
+list_locations() {
+    for location in $all_locations
+    do
+        entry_prefix=" "
+        if [ "${location}" == "${current_location}" ]
+        then
+            entry_prefix="✔"
+        fi
+        echo "${entry_prefix} ${location} | shell=networksetup | param1=-switchtolocation | param2=\"${location}\""
+        echo "-- Remove ${location} | shell=networksetup | param1=-deletelocation | param2=\"${location}\""
+    done
+}
+
+add_location() {
+    read -r -d '' prompt <<'EOF'
+    set locationName to text returned of (display dialog "Enter the name of the new location" default answer "")
+    return locationName
+EOF
+    location_name=$(osascript -e ${prompt})
+    if [ "${location_name}" != "" ]
     then
-        entry_prefix="✔"
+        networksetup -createlocation "${location_name}" populate
     fi
-    echo "${entry_prefix} ${location} | shell=networksetup | param1=-switchtolocation | param2=\"${location}\""
-done
+}
+
+if [ "${1}" == "add-location" ]; then
+    add_location
+else
+    echo "${VAR_SYMBOL} ${current_location}"
+    echo "---"
+    echo "Switch location"
+    echo "---"
+    list_locations
+    echo "---"
+    echo "Add new location | bash=$0 param1=add-location"
+fi
